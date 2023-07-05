e3c6aa72dab2a8f51074edc9d70c1553dbc17248
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-07-05 16:08:14 +0600

    counter slice implemented, hook and middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import { useSelector } from "react-redux";
-import { RootState } from "./redux/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const { count } = useSelector((state: RootState) => state.counter);
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+
   return (
     <>
       <div className="flex items-center gap-4 m-10">
         <button
           className="border border-green-600 rounded-md
          py-2 px-3"
+          onClick={() => dispatch(increment())}
         >
           Increment
+        </button>
+        <button
+          className="border border-red-600 rounded-md
+         py-2 px-3"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
+          Increment By Value
         </button>
         <div>{count}</div>
         <button
           className="border border-red-600 rounded-md
          py-2 px-3"
+          onClick={() => dispatch(decrement())}
         >
           Decrement
         </button>
